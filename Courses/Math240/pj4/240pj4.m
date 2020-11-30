f703c19ce01f31056227d3b79734dc7ca3e3ab69
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2020-11-30 08:06:43 -0500

    matlab4

---
+++ Courses/Math240/pj4/240pj4.m
@@ -0,0 +1,27 @@
+clear all;
+%Name Yizhan Ao MATH240 Project4
+%% Problem 1
+format rat 
+
+%(a)
+
+%% Problem 2
+format short 
+A = [163 34 -8; -522 -108 26; 990 210 -47]
+%(a)
+[P,D] = eig(A)
+
+%(b)
+P*D*inv(P)
+A
+%YEs they are identical
+
+%(c)
+
+%% Problem 3
+%(a)
+
+%% Problem 4
+
+%% Problem 5
+%(a)
