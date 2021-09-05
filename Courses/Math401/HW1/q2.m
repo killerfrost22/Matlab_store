2cd77df5e36ff877d080f4c7545cf8833320d510
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-09-05 18:00:42 -0400

    updates

--- Courses/Math401/HW1/q2.m
+++ Courses/Math401/HW1/q2.m
@@ -1,13 +1,17 @@
-M = [0.02 0 0.02 0.04;0 0.01 0.05 0.04;0.03 0 0.01 0.02;0.02 0.05 0.01 0];
+
+format short
+M = [0.02 0 0.02 0.04;0 0.01 0.05 0.04;0.03 0 0.01 0.02;0.02 0.05 0.01 0]
 judge = true;
 i = 1 ;
 temp = 0 ;
-sum = eye(4);
-while (flag)
+sum = eye(4)
+
+
+while (judge)
     sum = sum + M^i;
     if (temp == sum)
         disp(i);
-        flag = false;
+        judge = false;
     else
         temp = sum; 
         i = i+1;
