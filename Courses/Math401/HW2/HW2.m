7bc18778140fecf7df601f91ae037b0daf3dcaed
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-09-12 12:13:35 -0400

    2d

---
+++ Courses/Math401/HW2/HW2.m
@@ -0,0 +1,29 @@
+%% Yizhan Ao UID:116022064
+%% MATH401 HW2
+%% Linear Algebra and its Applications
+
+%% Question 1
+
+%% Question 2
+format long;
+x = 7, y =9
+r = sqrt(x^2+y^2)
+Q = atan(y/x)
+X = 7*sind(50)-9*sind(50)
+Y = 9*cosd(50)+7*sind(50)
+
+
+
+
+
+%% Question 3
+
+%% Question 4
+
+%% Question 5
+
+%% Question 6
+
+%% Question 7
+
+%% Question 8
